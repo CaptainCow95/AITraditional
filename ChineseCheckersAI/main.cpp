c807c0bf37a2e9f51461bc1cf1af484e4ad3cbde
Nathan Clarke <CaptainCow95@gmail.com>
2015-05-25 14:09:41 -0600

    Removing long versions of arguments

--- ChineseCheckersAI/main.cpp
+++ ChineseCheckersAI/main.cpp
@@ -6,20 +6,16 @@
 int main(int argc, char **argv)
 {
     std::cerr << "Additional arguments:" << std::endl;
-    std::cerr << "--name n                Sets the agents name to what is supplied in 'n'" << std::endl;
-    std::cerr << "-n n                    Sets the agents name to what is supplied in 'n'" << std::endl;
-    std::cerr << "--depth d               Sets the depth to play randomly to" << std::endl;
-    std::cerr << "-d d                    Sets the depth to play randomly to" << std::endl;
-    std::cerr << "--verbose               Prints out tree information after each move" << std::endl;
-    std::cerr << "-v                      Prints out tree information after each move" << std::endl;
-    std::cerr << "--explorationconstant   Sets the exploration constant of the UCB calculation" << std::endl;
-    std::cerr << "-e                      Sets the exploration constant of the UCB calculation" << std::endl;
+    std::cerr << "-n      Sets the agents name to what is supplied in 'n'" << std::endl;
+    std::cerr << "-d d    Sets the depth to play randomly to" << std::endl;
+    std::cerr << "-v      Prints out tree information after each move" << std::endl;
+    std::cerr << "-e      Sets the exploration constant of the UCB calculation" << std::endl;
 
     Agent a;
 
     for (int i = 1; i < argc; ++i)
     {
-        if (strcmp(argv[i], "--name") == 0 || strcmp(argv[i], "-n") == 0)
+        if (strcmp(argv[i], "-n") == 0)
         {
             ++i;
             if (i < argc)
@@ -32,7 +28,7 @@ int main(int argc, char **argv)
                 std::cerr << "Found argument to set the name, but no name followed." << std::endl;
             }
         }
-        else if (strcmp(argv[i], "--depth") == 0 || strcmp(argv[i], "-d") == 0)
+        else if (strcmp(argv[i], "-d") == 0)
         {
             ++i;
             if (i < argc)
@@ -45,12 +41,12 @@ int main(int argc, char **argv)
                 std::cerr << "Found argument to set the depth, but no depth followed." << std::endl;
             }
         }
-        else if (strcmp(argv[i], "--verbose") == 0 || strcmp(argv[i], "-v") == 0)
+        else if (strcmp(argv[i], "-v") == 0)
         {
             a.setVerbose(true);
             std::cerr << "Setting to verbose mode" << std::endl;
         }
-        else if (strcmp(argv[i], "--explorationconstant") == 0 || strcmp(argv[i], "-e") == 0)
+        else if (strcmp(argv[i], "-e") == 0)
         {
             ++i;
             if (i < argc)
